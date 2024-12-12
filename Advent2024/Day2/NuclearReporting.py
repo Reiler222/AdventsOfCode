2a5df6843d0a2a248aa2635c5178393f92d46069
Andrés Urraca <andres.urraca_barredo@contractors.roche.com>
2024-12-12 15:03:29 +0100

    Complete day 1, start day 2

---
+++ Advent2024/Day2/NuclearReporting.py
@@ -0,0 +1,17 @@
+filename = "input.txt"
+
+with open(filename, "r") as file:
+    for levels in file:
+        
+        values = levels.strip().split(" ")
+        
+        
+        
+        
+# Function for searching if the level report is OK or not  
+   
+def check_levels_report():
+    pass
+
+def is_ordered(lst, descending=False):
+    pass
